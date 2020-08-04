18a1ea4a900b5f0f4507856708ca062f1a3691ba
yinmeng <yinmeng@greatld.com>
2020-08-04 22:47:10 +0800

    .

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const responseBuilder = require('aws-lambda-response-builder')
-exports.handler = async (event) => {
+const wechat = require('./lib/wechat')
+const handler = async (event) => {
   // TODO implement
   console.log(event)
-  return responseBuilder.buildApiGatewayOkResponse({ event });
+  // 解析发送过来的消息
+  const message = await wechat.message(event.body)
+  switch (message.MsgType) {
+    default:
+      return {
+        statusCode: 200,
+        body: wechat.reply(`您发送了：${message.Content}`, message)
+      }
+  }
 };
+
+exports.handler = handler
