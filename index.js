e9eb58f3db51ec7545429ce6206c8d5b17009650
yinmeng <yinmeng@greatld.com>
2020-08-26 11:36:01 +0800

    commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const wechat = require('./lib/wechat')
+const wx = require('./lib/wxprocess')
 const handler = async (event) => {
   // TODO implement
   console.log(event)
-  // 解析发送过来的消息
-  const message = await wechat.message(event.body)
-  switch (message.MsgType) {
-    default:
-      return {
-        statusCode: 200,
-        body: wechat.reply(`您发送了：${message.Content}`, message)
-      }
+  // 解析发送过来的消息,然后分发
+  const body = event.body
+  const query = event.queryStringParameters
+  // 在此处进行分发
+  if (query.signature && body.includes('<xml>')) {
+    return wx.handler(body)
+  }
+  return {
+    statusCode: 200,
+    body: 'not found process event hanlder'
   }
 };
 
